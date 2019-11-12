5603ff1438fdb9f14c02811f6946ccaec0e48f12
dninemfive <11084010+dninemfive@users.noreply.github.com>
2019-11-12 14:17:13 -0800

    fucking around or smth

--- Source/FactionDefsExpanded/FactionDef/FactionDefME.cs
+++ Source/FactionDefsExpanded/FactionDef/FactionDefME.cs
@@ -9,22 +9,21 @@ namespace D9Extended
 {
     public class FactionDefME : DefModExtension
     {
-        // Apparel to resort to when spawning into an overly-cold map. Defaults to the base game's parka and tuque, respectively. Extended PawnKinds have a boolean useParkas if you want to override these, and they'll prioritize tagged apparel if it satisfies cold needs.
-        List<ThingDef> defaultParkas; 
-        List<ThingDef> defaultTuques;
         List<ThingDef> AllowedBuildingStuff;
         List<ThingDef> StuffWeights;
-        List<Thing> AllowedMortars;
-        List<Thing> AllowedTurrets;
+        List<ThingDef> AllowedMortars;
+        List<ThingDef> AllowedTurrets;
+        //some kind of custom siege blueprint generator, set whether supplies arrive via drop pod or caravan
         List<SettlementGenWeight> settlementBiases;
         PawnKindDef leaderPawnKind;
-        Pawn CustomFirstLeader;
+        PawnKindDef customFirstLeader; //to have a hyper-specific first leader but not have clones show up when they die. 
+        float mtbLeaderTermYears;
+        float leaderChanceToDieWhenLeaving;
         float NewLeaderRelatedToPreviousChance;
         List<FactionOpinion> factionOpinions;
         bool useVanillaPawnGroupKinds = true; //true: uses Normal and Trader; false: requests for these are intercepted
         List<TraderPawnGroupOverride> traderGroupOverrides;
         PawnKindDefME pawnKindDefaults;
-        //basic PawnKindDef data
         //some way to override settlement generator
         //Uniforms: override PawnKindDefs conditionally based on a class or smth
         //change FactionOpinion preset to more general FactionTicker, which can be overriden for fun effects
