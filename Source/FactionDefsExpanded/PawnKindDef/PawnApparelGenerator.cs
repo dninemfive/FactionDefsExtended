218f52a6d4d4403170abbb7ee563c14a65b19b6c
dninemfive <11084010+dninemfive@users.noreply.github.com>
2019-11-12 19:02:17 -0800

    give up on soft PawnApparelGenerator, rewriting from scratch

---
+++ Source/FactionDefsExpanded/PawnKindDef/PawnApparelGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace D9Extended
+{
+    public static class PawnApparelGenerator
+    {
+        //TODO: get min/max temperature based on race stats
+        private const float StartingMinTemperature = 12f;
+        private const float TargetMinTemperature = -40f;
+        private const float StartingMaxTemperature = 32f;
+        private const float TargetMaxTemperature = 30f;
+        private const float maxIterations = 3;
+    }
+}
